8beeb882ac227d3c8182a2ad494c7a7659960954
wushenshen <wushenshen@gmail.com>
2022-01-08 18:57:09 -0800

    update info

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -3,14 +3,13 @@ import React from 'react';
 const Info = () => {
   return (
     <div className="info">
-      This application allows people to make Ethereum payments towards a contract. 
+      This application allows people to make Ethereum payments towards a contract.
       Payments made to this contract are divided among specified recipients (i.e. payees).
       These payees are defined during contract creation.
-      <br/><br/>
+      <br /><br />
       Please connect to your MetaMask wallet in order to access the following application functionality:
-      <br/><br/>
+      <br /><br />
       <ul>
-        <li>View payee addresses</li>
         <li>Make payments via the payment form</li>
         <li>View the amount of your contributions (if you've made a payment)</li>
         <li>Request your share of the payments (if you're one of the payees)</li>
